2183f04a61e25a9a6b907807e0c81f7fb04ed42f
jtillil <77788257+jtillil@users.noreply.github.com>
2024-12-18 15:43:33 +0100

    Update tcga_LDA.R

--- publication/tcga_LDA.R
+++ publication/tcga_LDA.R
@@ -90,7 +90,7 @@ for (resolution in c(5)) {
   )
   borutares = list()
   save(borutares, file = saveroot)
-  boruta_TCGA(tcgadat_rnaseq, 1, method, importance, 500, 60, n_iterations, 1, saveroot)
+  boruta_TCGA(tcgadat_rnaseq, 1, method, importance, 500, 30, n_iterations, 1, saveroot)
   
   # #### build modules
   # igraph_network = upgrade_graph(tcga_breast_pr$network)
@@ -129,7 +129,7 @@ for (resolution in c(5)) {
   )
   borutares = list()
   save(borutares, file = saveroot)
-  boruta_TCGA(tcgadat_micro, 1, method, importance, 500, 60, n_iterations, 1, saveroot)
+  boruta_TCGA(tcgadat_micro, 1, method, importance, 500, 30, n_iterations, 1, saveroot)
 }
 
 
